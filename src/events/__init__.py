b3cbc148f663f7942fe06e0acfb454c811bffdb8
lukiwa <lukasz.iwan.99@gmail.com>
2021-03-17 22:09:03 +0100

    Added events module

---
+++ src/events/__init__.py
@@ -0,0 +1,3 @@
+from .event import Event
+from .event import Observer
+
---
+++ src/events/event.py
@@ -0,0 +1,23 @@
+
+#you need to inherit from this class if you want to be notified
+class Observer():
+    _observers = []
+    def __init__(self):
+        self._observers.append(self)
+        self._observables = {}
+    def observe(self, event_name, callback):
+        self._observables[event_name] = callback
+
+
+class Event():
+    def __init__(self, name, data, autofire = True):
+        self.name = name
+        self.data = data
+        if autofire:
+            self.fire()
+    def fire(self):
+        for observer in Observer._observers:
+            if self.name in observer._observables:
+                observer._observables[self.name](self.data)
+
+
