61cc302fff83f8cd37d94e6104dc07541487ce33
Maksymilian Kadukowski <maksymilian.kadukowski@gmail.com>
2021-03-21 00:07:45 +0100

    added uts

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='rpiplatesrecognition',
+    version='0.0.1',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.6',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
