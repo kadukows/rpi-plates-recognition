7873023f501935de860417703020fa50fd6cd3f7
lukiwa <lukasz.iwan.99@gmail.com>
2021-03-22 19:31:03 +0100

    Added gate controller + todo unit tests

--- src/rpiplatesrecognition/libs/gate/__init__.py
+++ src/rpiplatesrecognition/libs/gate/__init__.py
@@ -1,3 +1,4 @@
 from .gate_controller import GateController
 
-gate = GateController()
+gate = GateController(
+    gpio_pin=3, button_press_time_s=0.350, gate_opening_time_s=15)
--- src/rpiplatesrecognition/libs/gate/gate_controller.py
+++ src/rpiplatesrecognition/libs/gate/gate_controller.py
@@ -1,20 +1,60 @@
+from gpiozero import OutputDevice
+import datetime
+from time import sleep
+
+
 class GateController:
-    """
-    Wrapper for low level communication with gate.
+    def __init__(self, gpio_pin, button_press_time_s, gate_opening_time_s):
+        self.gpio_pin = gpio_pin
+        self.button_press_time = button_press_time_s
+        self.gate_opening_time = gate_opening_time_s
+        self.init_state_change_time = datetime.datetime.now(
+        ) - datetime.timedelta(seconds=self.gate_opening_time)
+        self.gpio_pin = OutputDevice(gpio_pin)
+        self.gpio_pin.off()
+        self.opened = False
+        self.is_closing = False
+        self.is_opening = False
 
-    ...
+    def __press_button(self):
+        self.gpio_pin.on()
+        sleep(self.button_press_time)
+        self.gpio_pin.off()
 
-    TODO
-    """
+    # check for busy state and update open/close state of the gate
+    def is_busy(self) -> bool:
+        if(datetime.datetime.now() < self.init_state_change_time + datetime.timedelta(seconds=self.gate_opening_time)):
+            return True
+        else:
+            if self.is_closing and not self.is_opening:
+                self.opened = False
+                self.is_closing = False
 
-    def __init__(self):
-        pass
+            elif self.is_opening and not self.is_closing:
+                self.opened = True
+                self.is_opening = False
 
-    def is_busy(self):
-        raise NotImplementedError
+            return False
 
+    def is_opened(self):
+        return self.opened
+
+    # it is safe to call each of the following without checking for is_busy or is_opened response
     def open(self):
-        raise NotImplementedError
+        if not self.is_busy():
+            if not self.opened:
+                self.__press_button()
+                self.is_opening = True
+                self.init_state_change_time = datetime.datetime.now()
 
     def close(self):
-        raise NotImplementedError
+        if not self.is_busy():
+            if self.opened:
+                self.__press_button()
+                self.opened = False
+                self.is_closing = True
+                self.init_state_change_time = datetime.datetime.now()
+
+    def stop(self):
+        if self.is_busy():
+            self.__press_button()
