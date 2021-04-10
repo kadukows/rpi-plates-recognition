e6bf57ab7d4a1e32d15fb12891ca08f53d35fcb8
ptroszcz <61745721+ptroszcz@users.noreply.github.com>
2021-04-10 23:01:39 +0200

    Added algorithm to search for signs on the plate

--- src/rpiplatesrecognition/libs/plate_acquisition/run.py
+++ src/rpiplatesrecognition/libs/plate_acquisition/run.py
@@ -1,2 +1,35 @@
+import numpy as np
+import cv2 as cv
+from edge_projection_algorithm import *
+from signs_extraction import *
+
+import os  # do usuniecia
+
+
+# this function convert image to recognized number plates string ( can be more than one )
+def photo_to_plate(img:np.ndarray):
+
+    # possible areas where plate can be recognized
+    possible_plates = edge_projection_algorithm(img)
+
+    segments = find_segments(possible_plates)
+    # segments need to be 2D list
+
+    # recognized_strings = recognize_plate(segments) # Tomek
+    recognized_strings = ["abc12345","cba54321"]
+
+    return recognized_strings
+
+def main():
+
+    directory = os.fsencode("images")
+    
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        img = cv.imread("images/"+filename)
+        photo_to_plate(img)
+
+
 if __name__ == "__main__":
     main()
+
