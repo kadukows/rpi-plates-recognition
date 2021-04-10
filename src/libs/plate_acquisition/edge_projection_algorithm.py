7ca76d75fd696ea9de764e8cf2f7682d8587a13c
Jakub Kedzierski <248915@student.pwr.edu.pl>
2021-04-10 18:52:00 +0200

    improved algorithm

--- src/libs/plate_acquisition/edge_projection_algorithm.py
+++ src/libs/plate_acquisition/edge_projection_algorithm.py
@@ -1,129 +1,189 @@
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
+
 
 import os  # do usuniecia
+
+img_size = (700, 550)
 
 
 def preprocessing_image(img: np.ndarray):
     gauss_kernel = (3, 3)
-    gauss_sigma = 3
-    img_size = (700, 550)
+    gauss_sigma = 7
 
-    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    img = cv.resize(img, img_size)
-    img = cv.medianBlur(img, 3)
+    img = cv.medianBlur(img, 5)
     img = cv.equalizeHist(img)
     img = cv.GaussianBlur(img, (gauss_kernel), gauss_sigma)
 
     return img
 
-
 def edge_detection(img: np.ndarray):
+    threshold_down = 50
+    morph_kernel_size = (50,12)
 
-    rectKernel = cv.getStructuringElement(cv.MORPH_RECT, (15, 8))
+    rectKernel = cv.getStructuringElement(cv.MORPH_RECT, morph_kernel_size)
     img = cv.morphologyEx(img, cv.MORPH_TOPHAT, rectKernel)
+    thresh,img = cv.threshold(img,threshold_down,255,cv.THRESH_TOZERO)
 
     return img
 
-
-def draw_projection(proj_x, proj_y, img):
+def draw_x_projection(proj_x,img):
     max_x = np.max(proj_x)
-    max_y = np.max(proj_y)
     width_proj = 500
-    height_proj = 500
 
     result = np.zeros((proj_x.shape[0], width_proj))
     for row in range(img.shape[0]):
         cv.line(result, (0, row), (int(proj_x[row] * width_proj / max_x), row), (255, 255, 255), 1)
 
+    cv.imshow("X Projection", result)
+    cv.waitKey()
+
+def draw_y_projection(proj_y,img):
+    height_proj = 500
+    max_y = np.max(proj_y)
+
     result_y = np.zeros((height_proj, proj_y.shape[0]))
     for col in range(img.shape[1]):
-        cv.line(result_y, (col, height_proj), (col, height_proj - int(proj_y[col] * height_proj / max_y)),
+        cv.line(result_y, (col, int(height_proj)), (col, int(height_proj - proj_y[col] * height_proj / max_y)),
                 (255, 255, 255), 1)
 
+    cv.imshow("Y Projection", result_y)
+    cv.waitKey()
 
-    cv.imshow("projection", result)
-    cv.imshow("projection y", result_y)
-
-
-def edge_projection_with_window_function(img: np.ndarray):
+def find_up_and_down_bound(img):
     mean_size_x = 15
-    mean_size_y = 30
-    height_percent = 0.7
-    max_height = 80
     possible_bounds = 3
-    possible_horizontal = 3
+    min_size_y = 50
+    max_height = 100
+    height_percent = 0.7
+
     lp_y_bounds = [] # (dolna granica, górna granica) ...
-    lp_x_bounds = []  # (lewa granica, prawa granica) ...
 
     proj_x = np.sum(img, 1)
     copy_x = proj_x.copy()
     proj_x[0:mean_size_x] = 0
     proj_x[len(proj_x) - mean_size_x:len(proj_x)] = 0
+
     for i in range(mean_size_x, len(proj_x) - mean_size_x):
         proj_x[i] = np.mean(copy_x[i - mean_size_x:i + mean_size_x])
 
+    projection_x_edit = proj_x.copy()
+    mean_proj = np.mean(proj_x)
 
-    proj_x_copy = proj_x.copy()
-    for i in range(0,possible_bounds):
-        index = np.unravel_index(proj_x_copy.argmax(), proj_x_copy.shape)[0]
+    for i in range(0, possible_bounds):
+        index = np.unravel_index(projection_x_edit.argmax(), projection_x_edit.shape)[0]
 
-        up_bound= index
-        for j in range(index,index + int(max_height/2)):
-            if proj_x_copy[j] < height_percent * proj_x_copy[index]:
+        up_bound = index + int(min_size_y / 2)  # default up bound
+
+        for j in range(index + int(min_size_y / 2), index + int(max_height / 2)):
+            if j >= img_size[1]:
+                break
+
+            if projection_x_edit[j] < height_percent * projection_x_edit[index]:
                 up_bound = j
                 break
 
-        down_bound= index
-        for j in range(index - int(max_height/2),index):
-            if proj_x_copy[j] < height_percent * proj_x_copy[index]:
+        down_bound = index - int(min_size_y / 2)
+        for j in range(index - int(min_size_y / 2), index - int(max_height / 2), -1):
+            if j <= 0:
+                break
+
+            if projection_x_edit[j] < height_percent * projection_x_edit[index]:
                 down_bound = j
                 break
 
-        proj_x_copy[index - 20: index + 20] = 0
-        lp_y_bounds.append((down_bound,up_bound))
+        projection_x_edit[down_bound: up_bound] = mean_proj
+        lp_y_bounds.append((down_bound, up_bound))
 
-    # found x values
+    return lp_y_bounds
+
+def get_bound_using_edge_projection(img: np.ndarray):
+    mean_size_y = 65
+    width_percent=0.45
+    max_width = 600
+
+    min_size_x = 120
+
+    lp_y_bounds = find_up_and_down_bound(img)
+    lp_x_bounds = []  # (lewa granica, prawa granica) ...
 
     proj_y = []
-    columns = []
-    for i in range(0, 3):
+    for i in range(0, len(lp_y_bounds)):
         proj_y = np.sum(img[lp_y_bounds[i][0]: lp_y_bounds[i][1]][:], 0)
-        copy_y = proj_y
-        copy_y[0:mean_size_y] = 0
-        copy_y[len(copy_y) - mean_size_y:len(copy_y)] = 0
+        mean_proj = np.mean(proj_y)
+        projection_y_edit = proj_y.copy()
+        projection_y_edit[0:mean_size_y] = 0
+        projection_y_edit[len(projection_y_edit) - mean_size_y:len(projection_y_edit)] = 0
 
-        for j in range(mean_size_y, len(copy_y) - mean_size_y):
-            copy_y[j] = np.mean(proj_y[j - mean_size_y:j + mean_size_y])
-
-        columns.append(np.unravel_index(proj_y.argmax(), proj_y.shape)[0])
-
-
-    draw_projection(proj_x, proj_y, img)
+        for j in range(mean_size_y, len(projection_y_edit) - mean_size_y):
+            projection_y_edit[j] = np.mean(proj_y[j - mean_size_y:j + mean_size_y])
 
 
-    for i in range(0,3):
-        cv.rectangle(img, (columns[i]-100, lp_y_bounds[i][0]), (columns[i]+100, lp_y_bounds[i][1]), (120, 120, 120))
-        cv.imshow("test2", img)
-        cv.waitKey()
+        index = np.unravel_index(projection_y_edit.argmax(), projection_y_edit.shape)[0]
+
+        right_bound = index + int(min_size_x/2)
+        for k in range(index + int(min_size_x/2), index + int(max_width / 2)):
+            if k >= img_size[0]:
+                break
+
+            if projection_y_edit[k] < width_percent * projection_y_edit[index]:
+                right_bound = k
+                break
+
+        left_bound = index - int(min_size_x/2)
+        for k in range(index - int(min_size_x/2), index - int(max_width / 2), -1):
+            if k <= 0:
+                break
+
+            if projection_y_edit[k] < width_percent * projection_y_edit[index] :
+                left_bound = k
+                break
+
+        projection_y_edit[left_bound: right_bound] = 0
+        lp_x_bounds.append((left_bound, right_bound))
 
 
-    return img
+    return lp_x_bounds,lp_y_bounds
 
 
-def chose_one_number_plate(proposed_areas):
-    return proposed_areas
+def chose_number_plate(proposed_areas,img):
+
+    lp_x_bounds = proposed_areas[0]
+    lp_y_bounds = proposed_areas[1]
+
+    best_fit = [(0,0),(0,1),(0,2)]
+
+    return best_fit
 
 
 def edge_projection_algorithm(img: np.ndarray):
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.resize(img, img_size)
+    img_copy = img.copy()
+
     img = preprocessing_image(img)
-
     img = edge_detection(img)
+    proposed_areas = get_bound_using_edge_projection(img)
 
-    proposed_areas = edge_projection_with_window_function(img)
+    lp_x_bounds = proposed_areas[0]
+    lp_y_bounds = proposed_areas[1]
 
-    img = chose_one_number_plate(proposed_areas)
+    best_fit = chose_number_plate(proposed_areas,img_copy)
+    possible_plate = []
 
-    cv.waitKey()
-    return img
+    for i in range(0,2):
+        best = best_fit[i]
+        cv.rectangle(img, (lp_x_bounds[best[0]][0], lp_y_bounds[best[1]][0]), (lp_x_bounds[best[0]][1], lp_y_bounds[best[1]][1]), (120, 120, 120))
+        cv.imshow("test2", img)
+        left = lp_x_bounds[best[0]][0]
+        right = lp_x_bounds[best[0]][1]
+
+        down = lp_y_bounds[best[1]][0]
+        up = lp_y_bounds[best[1]][1]
+
+        pic = img_copy[down:up,left:right]
+        possible_plate.append(pic)
+        cv.imshow("tablica",pic)
+        cv.waitKey()
+
+    return  possible_plate
--- src/libs/plate_acquisition/run.py
+++ src/libs/plate_acquisition/run.py
@@ -6,26 +6,19 @@ import os  # do usuniecia
 
 
 def photo_to_plate(img:np.ndarray):
-    img = edge_projection_algorithm(img)    
+    possible_plates = edge_projection_algorithm(img)
+
     return "LUB8890H"
 
-
 def main():
-    
-    #img = cv.imread("images/PKR50972.png")
-    #img = cv.imread("images/WA3230C.png")
-    img = cv.imread("images/LUB8890H.png")
-    photo_to_plate(img)
 
-    """
     directory = os.fsencode("images")
     
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         img = cv.imread("images/"+filename)
         photo_to_plate(img)
-    """
-    
+
 
 if __name__ == "__main__":
     main()
