dbc438305799c370549676e2af0e2dfe1c1078c1
ptroszcz <61745721+ptroszcz@users.noreply.github.com>
2021-04-11 12:31:17 +0200

    Upgraded algorithm

--- src/rpiplatesrecognition/libs/plate_acquisition/signs_extraction.py
+++ src/rpiplatesrecognition/libs/plate_acquisition/signs_extraction.py
@@ -5,9 +5,9 @@ import cv2 as cv
 
 def adaptive_threshhold(img:np.ndarray):
     #img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret,img=cv.threshold(img,100,255,cv.THRESH_BINARY)
+    #ret,img=cv.threshold(img,100,255,cv.THRESH_BINARY)
     #ret,img=cv.threshold(img,65,255,cv.THRESH_BINARY)
-    #img=cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,2)
+    img=cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,2)
     #img= cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
     img=cv.bitwise_not(img)
     return img
@@ -19,17 +19,22 @@ def  cca(img:np.ndarray):
     stats=out[2]
     signs=[]
 
+    #Assumptions:
+    #Character is at least 40% of the plate
+    #Character is up to 90% of the plate
+    #The ratio of the height to the width of the character is greater than 1.2
+
     for i in range(0,out[0]):
-        if stats[i][3]>(0.4*len(img)) and (stats[i][3]/stats[i][2])>1.20:
+        if stats[i][3]>(0.4*len(img)) and (stats[i][3]/stats[i][2])>1.20 and stats[i][3]<(0.9*len(img)):
             x=stats[i][0]
             y=stats[i][1]
-            img2=img[y:y+stats[i][3],x:x+stats[i][2]]
+            img2=img[y:y+stats[i][3],x:x+stats[i][2]+1]
             signs.append((x,y,img2))               
     
     signs.sort(key=lambda x:x[0])
     j=0
     while(j<len(signs)-1):
-        if (signs[j][1]-10>signs[j+1][1] or signs[j][1]+10<signs[j+1][1]):
+        if (signs[j][1]-10>signs[j+1][1] or signs[j][1]+10<signs[j+1][1] or len(signs[j+1][2])<len(signs[j][2])-10):
             del signs[j+1]
         else:
             j+=1
@@ -41,6 +46,28 @@ def  cca(img:np.ndarray):
     return only_signs
 
 
+def flood_fill(sign):
+    sign_floodfill=sign.copy()
+    h, w = sign.shape[:2]
+    mask = np.zeros((h+2,w+2), np.uint8)
+
+    cv.floodFill(sign_floodfill, mask, (0,0) , 255)
+
+    sign_floodfill=cv.bitwise_not(sign_floodfill)
+
+    floodfilled_sign=sign | sign_floodfill
+    return floodfilled_sign
+
+
+def sign_morphology(sign):
+    sign=cv.resize(sign,(int(len(sign[0])*2),int(len(sign)*2)))
+    kernel=np.ones((5,5),np.uint8)
+    opened_sign=cv.morphologyEx(sign,cv.MORPH_OPEN,kernel)
+    opened_sign=cv.morphologyEx(opened_sign,cv.MORPH_OPEN,kernel)
+    closed_sign=cv.morphologyEx(opened_sign,cv.MORPH_CLOSE,kernel)
+    #closed_sign=cv.morphologyEx(closed_sign,cv.MORPH_CLOSE,kernel)
+    ret,result_sign=cv.threshold(closed_sign,100,255,cv.THRESH_BINARY)
+    return result_sign
 
 
 def find_segments(possible_plates):
@@ -50,11 +77,15 @@ def find_segments(possible_plates):
 
     for img in possible_plates:
         img=adaptive_threshhold(img)
+        cv.imshow("test2",img)
+        cv.waitKey()
         signs=cca(img)
-        if len(signs)>=4 and len(signs)<=8:
+        cv.waitKey()
+        if len(signs)>=4 and len(signs)<=9:
             found_signs=signs #There is one license plate assumed
             break
     for i in found_signs:
+        i=sign_morphology(i)
         cv.imshow("test2",i)
         cv.waitKey()
 
