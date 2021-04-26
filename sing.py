ab609e7e8b37ca00ab1155e0cc89e59e9584447c
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2021-04-26 12:03:37 +0200

    added baza testowa

--- sing.py
+++ sing.py
@@ -1,5 +1,5 @@
 import cv2
 from pytesseract import image_to_string
-img = cv2.imread('tablice/0.png')
-plate = image_to_string(img, lang='eng',config='--psm 6')
+img = cv2.imread('2/8.png')
+plate = image_to_string(img,lang='eng',config='--psm 6')
 print('License plate: ', plate)
